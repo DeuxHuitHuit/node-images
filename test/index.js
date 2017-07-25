0060b38c2f0637392f65e27d98049a8e535a2843
nitriques <nitriques@users.noreply.github.com>
2017-07-24 22:11:49 -0400

    Add webp tests
    Picked from 6a9ee7202d
    Picked from 0710002ec4
    Picked from b30640c894
    Picked from b7e5eb656d
    Picked from 882f3eaf99
    Picked from 9a115ef7b2

--- test/index.js
+++ test/index.js
@@ -17,6 +17,9 @@ images("input.jpg")
     .resize( 200 )
     .save("output_new.jpg");
 
+images("input.webp")
+     .resize( 200 )
+     .save("output_webp.webp");
 
 images("input.jpg")
     .size( 200 )
@@ -26,6 +29,9 @@ images("input.gif")
     .resize( 200 )
     .save("output_new_gif.jpg");
 
+images("input.webp")
+    .resize( 200 )
+    .save("output_new_webp.jpg");
 
 images("input.gif")
     .size( 200 )
